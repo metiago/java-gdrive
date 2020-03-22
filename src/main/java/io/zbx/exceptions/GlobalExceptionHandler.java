1c5a1b10ab67ca2da925b813074b4f0a8f772024
tiago <tiago.souza@tinyeye.com>
2020-03-22 11:31:16 -0600

    Added validation for missing servlet params and body payload;

--- src/main/java/io/zbx/exceptions/GlobalExceptionHandler.java
+++ src/main/java/io/zbx/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package io.zbx.exceptions;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,7 +20,21 @@ public class GlobalExceptionHandler {
             errorDetails.setStatusCode(((GoogleJsonResponseException) e).getStatusCode());
             errorDetails.setErrorMessage(((GoogleJsonResponseException) e).getStatusMessage());
             return new ResponseEntity<>(errorDetails, HttpStatus.valueOf(((GoogleJsonResponseException) e).getStatusCode()));
-        } else {
+        }
+
+        if (e instanceof MethodArgumentNotValidException) {
+            errorDetails.setStatusCode(400);
+            errorDetails.setErrorMessage(e.getMessage());
+            return new ResponseEntity<>(((MethodArgumentNotValidException) e).getBindingResult(), HttpStatus.BAD_REQUEST);
+        }
+
+        if (e instanceof MissingServletRequestParameterException) {
+            errorDetails.setStatusCode(400);
+            errorDetails.setErrorMessage(e.getMessage());
+            return new ResponseEntity<>(((MethodArgumentNotValidException) e).getBindingResult(), HttpStatus.BAD_REQUEST);
+        }
+
+        else {
             errorDetails.setStatusCode(500);
             errorDetails.setErrorMessage("Sorry. Something went wrong. Try again later.");
             return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
